fa38a005ec0be6ff4974bb568760574b3393df21
VeKa-Work <ver_jj@yahoo.com>
2020-11-19 22:58:10 +0800

    Addition of toString and update viewSelfAccount output method

--- src/Main/Account.java
+++ src/Main/Account.java
@@ -32,8 +32,7 @@ public class Account {
         List<Account> accounts = getAllEmpCredential();
         for (Account account: accounts){
             if (account.getEmpID().equals(empID)){
-                System.out.printf("ID: %s\n", account.getEmpID());
-                System.out.printf("Password: %s\n", account.getEmpPassword());
+                System.out.println(account.toString());
             }
         }
     }
@@ -52,5 +51,11 @@ public class Account {
 
     private void setEmpPassword(String empPassword) {
         this.empPassword = empPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + empID + '\n' +
+                "Password: " + empPassword + '\n';
     }
 }
